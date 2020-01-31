b57bd19008c02c1b43040bcaab72f5986ea791f6
Baxi19 <randald1991@gmail.com>
2020-01-31 06:22:33 -0600

    Folder name changed

---
+++ Bill/src/Class/Main.java
@@ -0,0 +1,38 @@
+package Class;
+
+import Windows.W_Login;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.DocumentException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Baxi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     * @throws com.itextpdf.text.DocumentException
+     */
+    public static void main(String[] args) throws FileNotFoundException, DocumentException, BadElementException, IOException {
+        try {
+            File file = new File(".");  
+            File[] files = file.listFiles();  
+            System.out.println("Current dir : " + file.getCanonicalPath()); 
+            System.out.println("Current dir : " + file.getAbsolutePath()); 
+            System.out.println("Current dir : " + file.getPath());
+            System.out.println("Current dir : " + file.getParent());
+        } catch (Exception e) {
+        }
+        W_Login w = new W_Login();
+        w.setVisible(true);
+    }
+        
+
+   
+
+}
